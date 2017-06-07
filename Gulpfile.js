541db7afdb9229e2eefdb50d707b33fbcb615a70
Mario Duarte <djtsunamimario@gmail.com>
2017-06-07 22:57:12 +0100

    v3 experimental changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,42 +1,55 @@
 // ========================================================
-// Load in all the required libs =======================================
+// Load in all the required packages ===================================
 // ========================================================
-var gulp = require('gulp'),
-		browserSync = require('browser-sync').create(),
-		syncy = require('syncy'),
-		fs = require('fs-extra'),
-		gulpIf = require('gulp-if'),
-		sass = require('gulp-sass'),
-		sourcemaps = require('gulp-sourcemaps'),
-		autoprefixer = require('gulp-autoprefixer'),
-		runSequence = require('run-sequence'),
-		plumber = require('gulp-plumber'),
-		uglify = require('gulp-uglify'),
-		watch = require('gulp-watch'),
-		batch = require('gulp-batch'),
-		rename = require("gulp-rename"),
-		inline = require('gulp-inline'),
-		minifyCss = require('gulp-minify-css'),
-		zip = require('gulp-zip'),
-		util = require('gulp-util'),
-		del = require('del'),
-		stripCssComments = require('gulp-strip-css-comments'),
-		stripDebug = require('gulp-strip-debug'),
+var gulp = require('gulp'), //https://github.com/gulpjs/gulp/blob/master/docs/API.md
+		browserSync = require('browser-sync').create(), //https://browsersync.io/docs/gulp
+		syncy = require('syncy'), //https://github.com/mrmlnc/syncy
+		imagemin = require('gulp-imagemin'), //https://www.npmjs.com/package/gulp-imagemin
+		fs = require('fs-extra'), //https://www.npmjs.com/package/fs.extra
+		gulpIf = require('gulp-if'), //https://github.com/robrich/gulp-if
+		sass = require('gulp-sass'), //https://www.npmjs.com/package/gulp-sass
+		sourcemaps = require('gulp-sourcemaps'), //https://www.npmjs.com/package/gulp-sourcemaps
+		autoprefixer = require('gulp-autoprefixer'), //https://www.npmjs.com/package/gulp-autoprefixer
+		runSequence = require('run-sequence'), //https://www.npmjs.com/package/run-sequence
+		plumber = require('gulp-plumber'), //https://www.npmjs.com/package/gulp-plumber
+		uglify = require('gulp-uglify'), //https://www.npmjs.com/package/gulp-uglify
+		watch = require('gulp-watch'), //https://www.npmjs.com/package/gulp-watch
+		batch = require('gulp-batch'), //https://www.npmjs.com/package/gulp-batch
+		rename = require("gulp-rename"), //https://www.npmjs.com/package/gulp-rename
+		inline = require('gulp-inline'), //https://www.npmjs.com/package/gulp-inline
+		minifyCss = require('gulp-minify-css'), //https://www.npmjs.com/package/gulp-minify-css
+		zip = require('gulp-zip'), //https://www.npmjs.com/package/gulp-zip
+		util = require('gulp-util'), //https://www.npmjs.com/package/gulp-util
+		del = require('del'), //https://www.npmjs.com/package/del
+		stripCssComments = require('gulp-strip-css-comments'), //https://www.npmjs.com/package/gulp-strip-css-comments
+		stripDebug = require('gulp-strip-debug'), //https://www.npmjs.com/package/gulp-strip-debug
 		neat = require('node-neat').includePaths; // neat documentation can be found at: http://neat.bourbon.io/docs/latest/
 
 // ========================================================
 // Options and other variables
+// Modify these to fit your needs, remember to refer back to the packages documentation
 // ========================================================
 
+var archiveName = 'archive.zip';
+var stripCssOptions = true;
+
 var autoprefixeroptions = {
-		browsers: ['last 3 versions', 'IE 10', 'IE 11'],
-		cascade: false
+		browsers: ['last 3 versions', 'IE 8', 'IE 9', 'IE 10', 'IE 11'],
+		cascade: true
 };
 
 var sassOptions = {
 	outputStyle: 'compressed',
 	includePaths: require('node-neat').includePaths
 };
+
+var inlineOptions = {
+	base: './',
+	js: uglify,
+	css: minifyCss,
+	disabledTypes: ['svg', 'img'], // Only inline css and js and ignores images
+	ignore: ['src/assets/vendor/**.css', 'src/assets/vendor/**.js'] // add assets that you want to ignore the inline process, ex: jquery
+}
 
 // ========================================================
 // Help task that outputs into the console all the commands and their options =========
@@ -45,11 +58,12 @@ gulp.task('help', function() {
 	util.log(util.colors.cyan('========='));
 	util.log(util.colors.cyan('Gulp help - task list'));
 	util.log(util.colors.cyan('========='));
-	util.log(util.colors.magenta('gulp', util.colors.cyan('--build --sync --zip'), util.colors.white(' | optional parameters')));
+	util.log(util.colors.magenta('gulp', util.colors.cyan('--build --zip'), util.colors.white(' | optional parameters')));
 	util.log(util.colors.magenta('gulp build'));
-	util.log(util.colors.magenta('gulp watch', util.colors.cyan('--dev --zip'), util.colors.white(' | optional parameters')));
+	util.log(util.colors.magenta('gulp watch', util.colors.cyan('--dev --optimize --zip'), util.colors.white(' | optional parameters')));
 	util.log(util.colors.magenta('gulp zip'));
-	util.log(util.colors.cyan('For more information please read the readme.md file provided.'));
+	util.log(util.colors.magenta('gulp optImg'));
+	util.log(util.colors.cyan('For more information please read the readme.md file provided and for individual packages documentation see the Gulpfile.js file.'));
 });
 
 // ========================================================
@@ -59,12 +73,10 @@ gulp.task('default', function() {
 	if ( util.env.build == true ) {
 		gulp.start('build');
 	} else {
-		runSequence(['sass','autoprefixer', 'scripts'], 'dist')
-		if ( util.env.sync == true ) {
-			gulp.start('browserSync');
-		}
 		if ( util.env.zip == true ) {
-			gulp.start('zip');
+			runSequence(['sass', 'scripts'], ['dist'], 'zip')
+		} else {
+			runSequence(['sass', 'scripts'], 'dist')
 		}
 	}
 });
@@ -96,11 +108,25 @@ gulp.task('build', function(){
 		<!--</a>-->
 	</body>
 
-	<script type="text/javascript">
-		var add = document.getElementById('add');
-	</script>
+	<script src="js/main.min.js" charset="utf-8"></script>
 
 	</html>`;
+
+	var keyframesTemplate = `// ===========================
+	// This file is intended to hold the keyframes animations
+	// for the banner, this helps keeping the workflow clean
+	// ===========================
+
+	@keyframes fadeIn {
+		0% { opacity: 0; }
+		100% { opacity: 1; }
+	};
+
+	@keyframes fadeOut {
+		0% { opacity: 1; }
+		100% { opacity: 0; }
+	};
+	`;
 
 	var scssTemplate = `//Sets all variables
 
@@ -111,6 +137,8 @@ gulp.task('build', function(){
 
 	$width : 0px;
 	$height : 0px;
+
+	@import "_partials/keyframes";
 
 	html, body {
 		margin: 0;
@@ -135,14 +163,19 @@ gulp.task('build', function(){
 	}
 	}`
 
-	fs.mkdirs('scss/', function (err) {
+	var scriptTemlate = `// ==================
+	// Add your custom javascript bellow
+	// ==================
+	var add = document.getElementById('add');`;
+
+	fs.mkdirs('src/scss/', function (err) {
 		if (err) return console.error(err)
 		util.log(util.colors.blue('Built ', util.colors.white.underline('scss/'), ' successfully!'));
 	})
 
-	fs.mkdirs('scripts/vendor/', function (err) {
+	fs.mkdirs('src/assets/vendor/', function (err) {
 		if (err) return console.error(err)
-		util.log(util.colors.blue('Built ', util.colors.white.underline('scripts/vendor'), ' successfully!'));
+		util.log(util.colors.blue('Built ', util.colors.white.underline('assets/vendor'), ' successfully!'));
 	})
 
 	fs.mkdirs('css/', function (err) {
@@ -150,9 +183,19 @@ gulp.task('build', function(){
 		util.log(util.colors.blue('Built ', util.colors.white.underline('css/'), ' successfully!'));
 	})
 
-	fs.mkdirs('images/', function (err) {
+	fs.mkdirs('js/', function (err) {
 		if (err) return console.error(err)
-		util.log(util.colors.blue('Built ', util.colors.white.underline('images/'), ' successfully!'));
+		util.log(util.colors.blue('Built ', util.colors.white.underline('js/'), ' successfully!'));
+	})
+
+	fs.mkdirs('src/assets/', function (err) {
+		if (err) return console.error(err)
+		util.log(util.colors.blue('Built ', util.colors.white.underline('assets/'), ' successfully!'));
+	})
+
+	fs.outputFile('src/scripts/main.js', scriptTemlate, function(err){
+		if (err) return console.error(err)
+		util.log(util.colors.blue('Built ', util.colors.white.underline('main.js'), ' successfully!'));
 	})
 
 	fs.outputFile('index.html', indexTemplate, function (err) {
@@ -160,7 +203,12 @@ gulp.task('build', function(){
 		util.log(util.colors.blue('Built ', util.colors.white.underline('index.html'), ' successfully!'));
 	})
 
-	fs.outputFile('scss/style.scss', scssTemplate, function (err) {
+	fs.outputFile('src/scss/_partials/_keyframes.scss', keyframesTemplate, function (err) {
+		if (err) return console.error(err)
+		util.log(util.colors.blue('Built ', util.colors.white.underline('_partials/_keyframes.scss'), ' successfully!'));
+	})
+
+	fs.outputFile('src/scss/style.scss', scssTemplate, function (err) {
 		if (err) return console.error(err)
 		util.log(util.colors.blue('Built ', util.colors.white.underline('style.scss'), ' successfully!'));
 	})
@@ -172,9 +220,10 @@ gulp.task('build', function(){
 // ========================================================
 gulp.task('sass', function () {
 	if ( util.env.dev == true ) { var dev = false; } else { var dev = true; }
-	return gulp.src('./scss/**/*.scss')
+	return gulp.src('./src/scss/**/*.scss')
 		.pipe(sass(sassOptions).on('error', sass.logError))
-		.pipe(gulpIf(dev, stripCssComments()))
+		.pipe(gulpIf(dev, stripCssComments(stripCssOptions)))
+		.pipe(autoprefixer(autoprefixeroptions))
 		.pipe(sourcemaps.write('./css'))
 		.pipe(gulp.dest('./css'));
 });
@@ -184,7 +233,7 @@ gulp.task('sass', function () {
 // ========================================================
 gulp.task('scripts', function() {
 	if ( util.env.dev == true ) { var dev = false; } else { var dev = true; }
-	return gulp.src('./scripts/*.js')
+	return gulp.src('./src/scripts/*.js')
 		.pipe(plumber())
 		.pipe(gulpIf(dev, stripDebug()))
 		.pipe(uglify())
@@ -195,56 +244,58 @@ gulp.task('scripts', function() {
 });
 
 // ========================================================
-// autoprefixer task =============================================
-// ========================================================
-gulp.task('autoprefixer', function(){
-	gulp.src('css/style.css')
-			.pipe(autoprefixer(autoprefixeroptions))
-			.pipe(rename({
-				suffix: '.min'
-			}))
-			.pipe(gulp.dest('css'))
-});
-
-// ========================================================
 // Watch task =================================================
 // ========================================================
 gulp.task('watch', function(){
-	watch('scripts/*.js', batch(function (events, done) {
-			gulp.start('scripts', done);
+
+	watch('src/scripts/*.js', batch(function (events, done) {
+		gulp.start('scripts', done);
 	}));
-	watch('scss/**/*.scss', batch(function (events, done) {
-			gulp.start('sass', done);
+
+	watch('src/scss/**/*.scss', batch(function (events, done) {
+		gulp.start('sass', done);
 	}));
-	watch('css/style.css', batch(function(events, done) {
-		gulp.start('autoprefixer', done);
+
+	watch(['css/*.css', '*.html', 'assets/*', 'js/*'], batch(function(events, done) {
+		if ( util.env.zip == true ) {
+			runSequence('dist', 'zip', done);
+		} else {
+			gulp.start('dist', done);
+		}
 	}));
-	watch(['css/style.min.css', '*.html', '/images/*'], batch(function(events, done) {
-		gulp.start('dist', done);
-	}))
+
 	gulp.start('browserSync');
-	watch('scss/**/*.scss', ['sass']).on('change', browserSync.reload);
-	watch('scripts/*.js', ['scripts']).on('change', browserSync.reload);
-	if ( util.env.zip == true ) {
-		gulp.start('zip');
-	}
+	watch('index.html').on('change', browserSync.reload);
+
+});
+
+// ========================================================
+// OptImg task - this will optimize images for web use ========================
+// ========================================================
+gulp.task('optImg', function(){
+	gulp.src('./dist/assets/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('./dist/assets'))
 });
 
 // ========================================================
 // Dist task - syncs images and inlines the styling and scripts into the index.html =======
 // ========================================================
 gulp.task('dist', function(){
-		syncy(['./images/*'], './dist/').then(() => {done();}).catch((err) => {done(err);});
+
+		syncy(['src/assets/**'], './dist/', {verbose: false, base: 'src'}).then(function(){
+			// Run image optimize after files are synced if option is selected
+			if ( util.env.optimize == true ) {
+				gulp.start('optImg');
+			}
+		})
+		.catch(console.error);
+
 		gulp.src('index.html')
-			.pipe(inline({
-				base: './',
-				js: uglify,
-				css: minifyCss,
-				disabledTypes: ['svg', 'img'], // Only inline css and js
-				ignore: ['./css/style.css']
-			}))
-			.pipe(gulp.dest('dist/'));
-			util.log(util.colors.magenta('Dist successfully sync the images and inlined your styles and scripts into the dist/index.html'));
+			.pipe(inline(inlineOptions))
+			.pipe(gulp.dest('dist/'))
+			.pipe(browserSync.stream());
+
 });
 
 // ========================================================
@@ -256,13 +307,12 @@ gulp.task('zip', function(){
 
 gulp.task('createZip', function(){
 	gulp.src('dist/**')
-		.pipe(zip('archive.zip'))
+		.pipe(zip(archiveName))
 		.pipe(gulp.dest('dist'));
-		util.log(util.colors.magenta('Distribution zip created successfully...'));
 });
 
 gulp.task('cleanZip', function(){
-	return del(['dist/archive.zip']);
+	return del(['dist/'+archiveName]);
 });
 
 // ========================================================
--- readme.md
+++ readme.md
@@ -25,35 +25,67 @@ Run ```npm install``` on your terminal at the root of your project, this will in
 
 - gulp help
 - gulp build
-- gulp --dev --sync --zip
-- gulp watch --dev --zip
+- gulp --dev --zip
+- gulp watch --dev --optimize --zip
 - gulp zip
+- gulp optImg
+
+**You can also change task option in ```Gulpfile.js``` under Options and other variables.**
 
 ---
+## Gulp Tasks
+
 #### Gulp build
 
-This will create all the folders needed to the project and will create a index.html with the basic structure of the banner and  a standard  CM clicktag, it will also create a style.scss file with you base variables and some predefined styles.
+This will create all the folders needed to the project and will create a index.html with the basic structure of the banner and  a standard  CM clicktag.
+
+Here is the created folder structure:
+
+- index.html
+- /css
+- /js
+- /src
+	- /assets
+		- /vendor
+	- /scripts
+		- main.js
+	- /scss
+		- style.scss
+		- /_partials
+			- _keyframes.scss
 
 The idea is to speed up the boring stuff so that you can get faster to the meat of the build.
 
 #### Gulp and Gulp Watch
 
-Gulp will run the process once and the watcher will whatch for file changes and run the process.
-
---sync => the watch task does not use --sync as a parameter as this will run browser sync by default but can be used on the gulp task. This will serve the index file of the dist folder.
+Gulp will run the process once and the watcher will whatch for file changes and run the process with browser sync by default ,this will serve the index file of the dist folder.
 
 --dev => can be used in the gulp and watch task, when used will prevent the removal of console logs and comments in css and javascript for easier debug.
 
 --zip => can be used with he gulp and watch task, this will generate fresh zip files every time the task runs, this will also delete the previous generated zip, if you new need to keep a copy of the old zip you can do so by either changing the variable for the name of the zip of renaming the old zip file.
 
+--optimize => refer to the gulp optImg section bellow
+
 #### Gulp Zip
 
 This will only zip the contents of the dist folder, replacing any old zip files. Same process as described previously.
+
+#### Gulp optImg
+
+This will minify PNG, JPEG, GIF and SVG images with imagemin.
 
 #### One way sync
 You shouldn't make any changes directly on the dist folder and all its contents are generated and managed using the gulpfile.
 
 On top of this there is a one way sync enabled in the images folder making sure that if you delete any images this will update the images folder in dist.
+
+---
+
+## How to add 3th party ```.css``` and ```.js``` files
+
+You can add 3th party ```.css``` and ```.js``` files by placing these in ```src/assets/vendor/``` folder, any file in this folder will be ignored and not inlined in the index.html.
+
+**just remember that the end path of these files will be ```/dist/assets/vendor/```, here is an example,** ```<link rel="stylesheet" href="assets/vendor/external.css">```.
 
 ---
 
